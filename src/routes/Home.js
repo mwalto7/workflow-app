45e32f87e4483471c34ef9d8a8ded99b0b7efa65
Mason Walton <dev.mwalto7@gmail.com>
2017-11-16 14:02:18 -0600

    Fixed Navbar component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,7 +3,7 @@ import Layout from './Layout';
 
 const Home = () => (
   <Layout>
-    <h1>Home page!</h1>
+    <h1>Home Page!</h1>
   </Layout>
 );
 
--- src/routes/Navbar.js
+++ src/routes/Navbar.js
@@ -1,87 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Container, Menu } from 'semantic-ui-react';
 
-class Navbar extends Component {
-  state = { activeItem: '' };
+const style = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  margin: '0',
+  padding: '0',
+  minHeight: '7vh',
+};
 
-  handleClick = (e, { name }) => this.setState({ activeItem: name });
-
-  render() {
-    const { activeItem } = this.state;
-
-    return (
-      <Container>
-        <Menu borderless fluid pointing secondary stackable size="large">
-          <Menu.Menu position="left">
-            <Menu.Item
-              header
-              as={NavLink}
-              to="/"
-              index={1}
-              content="Workflow"
-            />
-            <Menu.Item
-              as={NavLink}
-              to="/about"
-              index={2}
-              content="About"
-              name="about"
-              active={activeItem === 'about'}
-              onClick={this.handleClick}
-            />
-            <Menu.Item
-              as={NavLink}
-              to="/features"
-              index={3}
-              content="Features"
-              name="features"
-              active={activeItem === 'features'}
-              onClick={this.handleClick}
-            />
-            <Menu.Item
-              as={NavLink}
-              to="/pricing"
-              index={4}
-              content="Pricing"
-              name="pricing"
-              active={activeItem === 'pricing'}
-              onClick={this.handleClick}
-            />
-            <Menu.Item
-              as={NavLink}
-              to="/contact"
-              index={5}
-              content="Contact us"
-              name="contact"
-              active={activeItem === 'contact'}
-              onClick={this.handleClick}
-            />
-          </Menu.Menu>
-          <Menu.Menu position="right">
-            <Menu.Item
-              as={NavLink}
-              to="/register"
-              index={6}
-              content="Sign up"
-              name="register"
-              active={activeItem === 'register'}
-              onClick={this.handleClick}
-            />
-            <Menu.Item
-              as={NavLink}
-              to="/login"
-              index={7}
-              content="Log in"
-              name="login"
-              active={activeItem === 'login'}
-              onClick={this.handleClick}
-            />
-          </Menu.Menu>
-        </Menu>
-      </Container>
-    );
-  }
-}
+const Navbar = () => (
+  <Container style={style}>
+    <Menu borderless fluid pointing secondary stackable size="large">
+      <Menu.Menu position="left">
+        <Menu.Item
+          header
+          as={NavLink}
+          exact
+          to="/"
+          index={1}
+          content="Workflow"
+        />
+        <Menu.Item as={NavLink} to="/about" index={2} content="About" />
+        <Menu.Item as={NavLink} to="/features" index={3} content="Features" />
+        <Menu.Item as={NavLink} to="/pricing" index={4} content="Pricing" />
+        <Menu.Item as={NavLink} to="/contact" index={5} content="Contact us" />
+      </Menu.Menu>
+      <Menu.Menu position="right">
+        <Menu.Item as={NavLink} to="/register" index={6} content="Sign up" />
+        <Menu.Item as={NavLink} to="/login" index={7} content="Log in" />
+      </Menu.Menu>
+    </Menu>
+  </Container>
+);
 
 export default Navbar;
