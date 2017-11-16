a42758d972bf18efae1b5b9fb0a52843fc31df80
mchri18 <mchri18@lsu.edu>
2017-11-16 14:25:42 -0600

    home page layout

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Segment, Container, Button, Header, Icon } from 'semantic-ui-react';
+import {
+  Segment,
+  Container,
+  Button,
+  Header,
+  Icon,
+  Grid,
+  Image,
+} from 'semantic-ui-react';
 import Layout from './Layout';
 
 const Home = () => (
   <Layout>
-    <Segment
-      textAlign="center"
-      style={{ minHeight: 700, padding: '1em 0em' }}
-      vertical
-    >
+    <Segment textAlign="center" vertical>
       <Container text>
         <Header
           as="h1"
@@ -33,6 +37,28 @@ const Home = () => (
           <Icon name="right arrow" />
         </Button>
       </Container>
+    </Segment>
+    <Segment style={{ padding: '4em 0em' }} vertical>
+      <Grid container stackable verticalAlign="middle">
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Header as="h3" style={{ fontSize: '2em' }}>
+              We enable companies to improve their Workflow.
+            </Header>
+            <p style={{ fontSize: '1.33em' }}>
+              We give companies the ability to quickly and easily monitor
+              employees as well as assign them to teams and projects. Improve
+              wrokflow by having complete control from one location.
+            </p>
+          </Grid.Column>
+          <Grid.Column floated="right" width={6}>
+            <Image
+              size="large"
+              src="/assets/Workflow.png"
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Segment>
   </Layout>
 );
