d633d4706b263fe7abf8d561d4a6abbd902e1f21
kvellian <kvellian@gmail.com>
2017-11-26 17:06:45 -0600

    Added Different Fields in team page

--- src/containers/ViewTeam.js
+++ src/containers/ViewTeam.js
@@ -5,21 +5,26 @@ import Teams from '../components/TeamPageLayout/Teams';
 import Header from '../components/TeamPageLayout/Header';
 import Messages from '../components/TeamPageLayout/Messages';
 import Input from '../components/TeamPageLayout/Input';
+import SendMessage from '../components/TeamPageLayout/SendMessage';
 import AppLayout from '../components/TeamPageLayout/AppLayout';
 
 export default () => (
-  <AppLayout>
-    <Teams>Teams</Teams>
-    <Channels>Channels</Channels>
-    <Header>Header</Header>
-    <Messages>
-      <ul className="message-list">
-        <li />
-        <li />
-      </ul>
-    </Messages>
-    <Input>
-      <input type="text" placeholder="CSS Grid Layout Module" />
-    </Input>
-  </AppLayout>
-);
+    <AppLayout>
+      <Teams teams={[{ id: 1, letter: 'B' }, { id: 2, letter: 'Q' }]} />
+      <Channels
+        teamName="Team name"
+        username="Username"
+        channels={[{ id: 1, name: 'general' }, { id: 2, name: 'random' }]}
+        users={[{ id: 1, name: 'WorkflowHelper' }, { id: 2, name: 'user1' }]}
+      />
+      <Header channelName="general" />
+      <Messages>
+        <ul className="message-list">
+          <li />
+          <li />
+        </ul>
+      </Messages>
+      <SendMessage channelName="general" />
+    </AppLayout>
+  );
+  
