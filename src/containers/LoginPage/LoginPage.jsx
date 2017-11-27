72e77d84ef031bf88640a2cfcd5ff37589181514
mchri18 <mchri18@lsu.edu>
2017-11-27 17:13:42 -0600

    added back button to sign up and log in pages

--- src/containers/LoginPage/LoginPage.jsx
+++ src/containers/LoginPage/LoginPage.jsx
@@ -1,32 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Header, Message, Grid } from 'semantic-ui-react';
+import { Header, Message, Grid, Icon } from 'semantic-ui-react';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import './LoginPage.css';
 
 const LoginPage = () => (
-  <div className="LoginPage">
-    <Grid
-      columns={1}
-      centered
-      style={{ height: '100vh' }}
-      verticalAlign="middle"
-    >
-      <Grid.Column
-        mobile={16}
-        tablet={8}
-        computer={6}
-        textAlign="center"
+  <div>
+    <Link to="/">
+      <Icon
+        name="arrow circle left"
+        size="massive"
+        link
+        style={{ position: 'fixed', padding: '.125em', color: 'black' }}
+      />
+    </Link>
+    <div className="LoginPage">
+      <Grid
+        columns={1}
+        centered
+        style={{ height: '100vh' }}
         verticalAlign="middle"
       >
-        <Header as="h1" content="Log in" />
-        <LoginForm />
-        <Message>
-          New to Workflow?&nbsp;
-          <Link to="/register">Register</Link>&nbsp;instead.
-        </Message>
-      </Grid.Column>
-    </Grid>
+        <Grid.Column
+          mobile={16}
+          tablet={8}
+          computer={6}
+          textAlign="center"
+          verticalAlign="middle"
+        >
+          <Header as="h1" content="Log in" style={{ fontSize: '6em' }} />
+          <LoginForm />
+          <Message>
+            New to Workflow?&nbsp;
+            <Link to="/register">Register</Link>&nbsp;instead.
+          </Message>
+        </Grid.Column>
+      </Grid>
+    </div>
   </div>
 );
 
--- src/containers/RegistrationPage/RegistrationPage.jsx
+++ src/containers/RegistrationPage/RegistrationPage.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Header, Message, Grid } from 'semantic-ui-react';
+import { Header, Message, Grid, Icon } from 'semantic-ui-react';
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
 import './RegistrationPage.css';
 
 const RegistrationPage = () => (
-  <div className="RegistrationPage">
-    <Grid
-      columns={1}
-      centered
-      style={{ height: '100vh' }}
-      verticalAlign="middle"
-    >
-      <Grid.Column
-        mobile={16}
-        tablet={8}
-        computer={6}
-        textAlign="center"
+  <div>
+    <Link to="/">
+      <Icon
+        className="backButton"
+        name="arrow circle left"
+        size="massive"
+        link
+        style={{ position: 'fixed', padding: '.125em', color: 'black' }}
+      />
+    </Link>
+    <div className="RegistrationPage">
+      <Grid
+        columns={1}
+        centered
+        style={{ height: '100vh' }}
         verticalAlign="middle"
       >
-        <Header as="h1" content="Sign Up" />
-        <RegistrationForm />
-        <Message>
-          Already signed up?&nbsp;
-          <Link to="/login">Log in</Link>&nbsp;instead.
-        </Message>
-      </Grid.Column>
-    </Grid>
+        <Grid.Column
+          mobile={16}
+          tablet={8}
+          computer={6}
+          textAlign="center"
+          verticalAlign="middle"
+        >
+          <Header as="h1" content="Sign Up" style={{ fontSize: '6em' }} />
+          <RegistrationForm />
+          <Message>
+            Already signed up?&nbsp;
+            <Link to="/login">Log in</Link>&nbsp;instead.
+          </Message>
+        </Grid.Column>
+      </Grid>
+    </div>
   </div>
 );
 
