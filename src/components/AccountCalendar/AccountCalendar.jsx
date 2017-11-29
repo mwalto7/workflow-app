dcb739e775041bfc35d787b36a9625837be25eda
mchri18 <mchri18@lsu.edu>
2017-11-28 20:22:57 -0600

    edited calendar page

--- src/components/AccountCalendar/AccountCalendar.jsx
+++ src/components/AccountCalendar/AccountCalendar.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Icon } from 'semantic-ui-react';
 import './AccountCalendar.css';
 
 const AccountCalendar = () => (
   <div className="calendar">
     <header className="CalendarHeader">
+      <Icon
+        link
+        name="arrow circle left"
+        size="big"
+        style={{ margin: '.6em', padding: '.5em' }}
+      />
       <h1>November 2017</h1>
+      <Icon
+        link
+        name="arrow circle right"
+        size="big"
+        style={{ padding: '.5em' }}
+      />
     </header>
 
     <ul className="list weekdays">
@@ -37,23 +50,23 @@ const AccountCalendar = () => (
       <li className="listItem month=prev">31</li>
       <li className="listItem">
         1 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         2 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         3 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         4 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         5 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         6 <br />
@@ -65,95 +78,95 @@ const AccountCalendar = () => (
       </li>
       <li className="listItem">
         8 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         9 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         10 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         11 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         12 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         13 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         14 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         15 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         16 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         17 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         18 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         19 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         20 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         21 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         22 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         23 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         24 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         25 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         26 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         27 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         28 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         29 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem">
         30 <br />
-        <p></p>
+        <p />
       </li>
       <li className="listItem month-next">1</li>
       <li className="listItem month-next">2</li>
