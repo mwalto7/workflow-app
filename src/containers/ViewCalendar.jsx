4c5a1370183bf3f85c3e1ea9be2137bf8a1db064
mchri18 <mchri18@lsu.edu>
2017-11-28 19:24:32 -0600

    created navbar for account as well as connected and styled pages

---
+++ src/containers/ViewCalendar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AccountNavbar from '../components/AccountNavbar/AccountNavbar';
+import AccountCalendar from '../components/AccountCalendar/AccountCalendar';
+
+const ViewSchedule = () => (
+  <div>
+    <AccountNavbar />
+    <AccountCalendar/>
+  </div>
+);
+
+export default ViewSchedule;
